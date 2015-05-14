beb85e300012d8cfffb5395351d74526faac370d
Luis Tellez <luistellez@gmail.com>
2015-05-14 08:19:29 -0430

    Add Other Lugar and Posada.js

--- Posadas.WebUI/App_Start/BundleConfig.cs
+++ Posadas.WebUI/App_Start/BundleConfig.cs
@@ -26,6 +26,11 @@ namespace Posadas.WebUI
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/posada").Include(
+                       "~/Scripts/app/posada.js"));
+
+
         }
     }
 }
---
+++ Posadas.WebUI/Scripts/App/posada.js
@@ -0,0 +1,32 @@
+﻿var uri = '/api/Lugares/';
+
+$(document).ready(function () {
+    $("#OtroLugar").hide();
+    $('#EstadoId').on('change', function () {
+        $("#OtroLugar").hide();
+        $.getJSON(uri + this.value)
+        .done(function (data) {
+            // On success, 'data' contains a list of products.
+            $('#LugarId').removeAttr("disabled").find('option').remove();
+            $('#LugarId').append('<option value="0">Seleccione un lugar</option>');
+
+            $.each(data, function (key, item) {
+                // Add a list item for the product.
+
+                $('#LugarId').append('<option value="' + item.Id + '">' + item.Nombre + '</option>');
+
+            });
+            $('#LugarId').append('<option value="-1"> otro </option>');
+        });
+
+    });
+
+    $('#LugarId').on('change', function () {
+        if (this.value == -1) {
+            $("#OtroLugar").show();
+        }
+        else {
+            $("#OtroLugar").hide();
+        }
+    });
+});
