f0d0e3dcfd48b7cb1c2b837041c4157677d71b58
Luis Tellez <luistellez@gmail.com>
2015-06-10 23:46:10 -0430

    Add Plan Entity

---
+++ Posadas.Domain/Entities/Plan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posadas.Domain.Entities
+{
+    public class Plan:BaseEntity
+    {
+        public string Nombre { get; set; }
+        public int Precio { get; set; }
+        public int NumImagenes { get; set; }
+
+    }
+}
